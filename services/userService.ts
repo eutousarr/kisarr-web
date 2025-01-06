d0fe55d35def54a1f0763a0ab121c429c3f441ef
Ibrahima Sarr <kognary@gmail.com>
2025-01-06 01:06:50 +0000

    update userService

--- services/userService.ts
+++ services/userService.ts
@@ -29,7 +29,6 @@ export const addUser = async(clerkUserId: string,userUsername: string, userName:
   }
 }
 
-
 export const getUser = async(clerkUserId: string) => {
   try {
     const user = await prisma.user.findUnique({
