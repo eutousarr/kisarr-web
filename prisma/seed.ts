dea3d4d3005355c5794f09b87b5255928c861c31
Ibrahima Sarr <eutou.sarrene@gmail.com>
2025-01-08 13:07:32 +0000

    Update again seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,44 +4,41 @@ const prisma = new PrismaClient()
 
 const userData: Prisma.UserCreateInput[] = [
   {
-    userName: 'Alice',
-    userUsername: 'alice',
-    userEmail: 'alice@prisma.io',
+    "id" : "5b504d36-0622-42a8-9365-14823110db5d",
+    "userName": "Ibrahima Sarr",
+    userUsername: 'kisarrweb',
+    userEmail: "kisarrweb@gmail.com",
     invoices: {
       create: [
         {
-          name: 'Boutique',
+          "id" : "be3b3a",
+          "name" : "Alassane Janvier 2025",
+          "issuerName" : "Boutique Alassane",
+          "issuerAddress" : "Bignona, 75 331 54 27",
+          "clientName" : "Kis@rrWeb s\/c Ibrahima Sarr",
+          "clientAddress" : "Bignona - 77 554 41 91",
+          "invoiceDate" : "2024-12-28",
+          "dueDate" : "2025-01-30",
+          "vatActive" : false,
+          "vatRate" : 20.0,
+          "status" : 1,
+        },
+        {
+         "id" : "44f9c3",
+      		"name" : "Janvier 2025",
+      		"issuerName" : "Woury Diallo",
+      		"issuerAddress" : "Bignona, 78 307 14 88\nBoutique \"Détail en herbe\"",
+      		"clientName" : "Kis@rrWeb",
+      		"clientAddress" : "Quartier Manguiline Plateau, Bignona\n77 554 41 91",
+      		"invoiceDate" : "2025-01-02",
+      		"dueDate" : "2025-01-30",
+      		"vatActive" : true,
+      		"vatRate" : 20.0,
+      		"status" : 5,
         },
       ],
     },
-  },
-  {
-    userName: 'Nilu',
-    userUsername: 'nilu',
-    userEmail: 'nilu@prisma.io',
-    invoices: {
-      create: [
-        {
-          name: 'Prisma on Twitter',
-        },
-      ],
-    },
-  },
-  {
-    userName: 'Mahmoud',
-    userUsername: 'moha',
-    userEmail: 'mahmoud@prisma.io',
-    invoices: {
-      create: [
-        {
-          name: 'Prisma on GitHub',
-        },
-        {
-          name: 'Prisma on YouTube',
-        },
-      ],
-    },
-  },
+  }
 ]
 
 async function main() {
