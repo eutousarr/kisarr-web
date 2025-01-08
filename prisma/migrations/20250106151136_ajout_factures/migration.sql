48bd0d3b951e41c986390ed230b6579d43459740
Ibrahima Sarr <eutou.sarrene@gmail.com>
2025-01-08 14:16:21 +0000

    Update migration.sql

--- prisma/migrations/20250106151136_ajout_factures/migration.sql
+++ prisma/migrations/20250106151136_ajout_factures/migration.sql
@@ -4,11 +4,6 @@
   - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- DropTable
-DROP TABLE "Post";
 
 -- CreateTable
 CREATE TABLE "Invoice" (
